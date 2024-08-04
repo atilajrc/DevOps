3f51de3f425c99e83ad890f78f33a3e2d90c0a20
atilajrc <atilajrc@gmail.com>
2024-08-04 07:34:38 -0300

    Commit

---
+++ Python/variaveis.py
@@ -0,0 +1,32 @@
+nome_usuario = 'Atila Costa'
+print(nome_usuario)
+
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+
+print(media)
+print(n2)
+
+nome, idade = 'Atila', 47
+
+# Função type()
+print(type(media))
+
+estado = True
+
+print(type(estado))
+
+print(type(1+2j))
+
+#Função isinstance
+
+a = 100
+b = 'sol'
+
+print(isinstance(a, int))
+
+a = 40
+c = 3
+r = a * c
+
+print(r)
