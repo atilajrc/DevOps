ba2f67a50713fc16b95c2b34411afa65682e5a6c
atilajrc <atilajrc@gmail.com>
2024-09-06 11:21:32 -0300

    commit

---
+++ Vagrant/Vagrant_VirtualBox/geral.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Update e Upgrade
+sudo apk update
+sudo apk upgrade
+
+#Install Apps
+sudo apk add neofetch net-tools btop wget curl nano ncftp git python3
+
+#Copiar Chave SSH Ansible
+#cat key.pub >> /home/vagrant/.ssh/authorized_keys
+#rm key.pub
